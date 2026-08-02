8dfbe7986a8a90713c8513ea32b164b2f0cb1997
Ananya Gupta <guptaananya1445@gmail.com>
2026-08-02 19:35:32 +0530

    Learning 2D Arrays

--- MultiDimensionalArrays/P10RotateBy90Degree.java
+++ MultiDimensionalArrays/P10RotateBy90Degree.java
@@ -2,7 +2,40 @@ package MultiDimensionalArrays;
 
 public class P10RotateBy90Degree {
     public static void main(String[] args) {
-             int a = 0;
+
+        int[][] arr = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        int n = arr.length;
+
+        // Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
         }
+
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[i][n - j - 1];
+                arr[i][n - j - 1] = temp;
+            }
+        }
+
+        // Print the rotated matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("DONE");
     }
 }
